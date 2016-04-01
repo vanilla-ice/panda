43b83d3d94d1e8531c84b7735d2e01f491814435
vanilla-ice <valeraerohin97@mail.ru>
2016-04-01 08:27:27 +0300

    version without slider

--- app/initialize.js
+++ app/initialize.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   inlineSVG.init();
-
+  window.initMap = () => {
+  let cairo, map;
+  cairo = {
+    lat: 55.745569,
+    lng: 37.662330
+  };
+   map = new google.maps.Map(document.getElementById('map'), {
+    scaleControl: true,
+    center: cairo,
+    zoom: 16
+  });
+    map.setOptions({
+    scrollwheel: false
+  });
+};
   console.log('Initialized app');
 });
